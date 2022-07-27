26b97a14a2b219a14bd461bc60e7aef0dd05f8c8
eduard hosu <ehosu@student.42wolfsburg.de>
2022-07-27 16:32:53 +0200

    added some tests in the main

--- M04/ex03/main.cpp
+++ M04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ehosu <ehosu@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 17:31:38 by ehosu             #+#    #+#             */
-/*   Updated: 2022/07/24 17:33:57 by ehosu            ###   ########.fr       */
+/*   Updated: 2022/07/27 16:07:38 by ehosu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,27 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 #include "ICharacter.hpp"
+#include "MateriaSource.hpp"
+#include "Character.hpp"
 
 
 int main()
 {
-	
-	std::cout << "Here I am" << std::endl;
+	MateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	ICharacter* me = new Character("me");
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	ICharacter* bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+
 	return 0;
 }
