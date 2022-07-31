01ae37f62649e5837fcdf118f5f99d420d4fff94
eduard hosu <ehosu@student.42wolfsburg.de>
2022-07-31 10:41:16 +0200

    files from M05 moved to ex00

---
+++ M05/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehosu <ehosu@student.42wolfsburg.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/29 18:50:54 by ehosu             #+#    #+#             */
+/*   Updated: 2022/07/30 11:31:28 by ehosu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	try
+	{
+		Bureaucrat b1("Bob", 12);
+		// Bureaucrat b2("Bob", -1);
+
+		std::cout << b1;
+		// b2.incGrade(2);
+		// std::cout << b2;
+	}
+	catch(const Bureaucrat::gradeTooLowException& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	catch(const Bureaucrat::gradeTooHighException& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
